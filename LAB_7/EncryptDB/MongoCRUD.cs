a44ba94fe06ca68ab40e3e5c98ec26631a3c7e41
PuscasDumitru <71260186+PuscasDumitru@users.noreply.github.com>
2021-12-20 21:13:01 +0200

    Add files via upload

---
+++ LAB_7/EncryptDB/MongoCRUD.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncryptDB
+{
+     public class MongoCRUD
+     {
+          private IMongoDatabase db;
+
+          public MongoCRUD(string database)
+          {
+               var client = new MongoClient();
+               db = client.GetDatabase(database);
+          }
+
+          public void InsertRecord<T>(string table, T record)
+          {
+               var collection = db.GetCollection<T>(table);
+               collection.InsertOne(record);
+          }
+
+          public List<T> LoadRecords<T>(string table)
+          {
+               var collection = db.GetCollection<T>(table);
+               return collection.Find(new BsonDocument()).ToList();
+          }
+     }
+}
